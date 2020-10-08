aa94f84fe0e69b36979c1aa5c524d82fcac26f90
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-10-08 22:10:13 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arraySymbol = str.split('');
+  let stack = [];
+  let brackets = Object.fromEntries(bracketsConfig);
+  for (let i = 0; i < arraySymbol.length; i++) {
+    if (stack[stack.length - 1] === arraySymbol[i]) {
+        stack.pop();
+    } else if (brackets[arraySymbol[i]]) {
+        stack.push(brackets[arraySymbol[i]]);
+    } else {
+        return false;
+    }
+  }
+  return stack.length === 0;
 }
+
